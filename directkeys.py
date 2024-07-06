f955d3aaab13b9e6288bb3e457404ee2689ebd0d
R_erik <eramosq@unsa.edu.pe>
2024-07-06 11:53:16 -0500

    Cambio del colo de deteccion a amarillo

--- directkeys.py
+++ directkeys.py
@@ -1,18 +1,22 @@
+# direct inputs
+# source to this solution and code:
+# http://stackoverflow.com/questions/14489013/simulate-python-keypresses-for-controlling-a-game
+# http://www.gamespp.com/directx/directInputKeyboardScanCodes.html
+
 import ctypes
 import time
 
 SendInput = ctypes.windll.user32.SendInput
 
-# Teclas escaneadas:
+
 W = 0x11
 A = 0x1E
 S = 0x1F
 D = 0x20
 Space = 0x39
 
-# Código C para la estructura INPUT
+# C struct redefinitions
 PUL = ctypes.POINTER(ctypes.c_ulong)
-
 class KeyBdInput(ctypes.Structure):
     _fields_ = [("wVk", ctypes.c_ushort),
                 ("wScan", ctypes.c_ushort),
@@ -30,28 +34,36 @@ class MouseInput(ctypes.Structure):
                 ("dy", ctypes.c_long),
                 ("mouseData", ctypes.c_ulong),
                 ("dwFlags", ctypes.c_ulong),
-                ("time", ctypes.c_ulong),
+                ("time",ctypes.c_ulong),
                 ("dwExtraInfo", PUL)]
 
 class Input_I(ctypes.Union):
     _fields_ = [("ki", KeyBdInput),
-                ("mi", MouseInput),
-                ("hi", HardwareInput)]
+                 ("mi", MouseInput),
+                 ("hi", HardwareInput)]
 
 class Input(ctypes.Structure):
     _fields_ = [("type", ctypes.c_ulong),
                 ("ii", Input_I)]
 
+# Actuals Functions
+
 def PressKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
-    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(1), ii_)
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
 
 def ReleaseKey(hexKeyCode):
     extra = ctypes.c_ulong(0)
     ii_ = Input_I()
-    ii_.ki = KeyBdInput(0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra))
-    x = Input(ctypes.c_ulong(1), ii_)
+    ii_.ki = KeyBdInput( 0, hexKeyCode, 0x0008 | 0x0002, 0, ctypes.pointer(extra) )
+    x = Input( ctypes.c_ulong(1), ii_ )
     ctypes.windll.user32.SendInput(1, ctypes.pointer(x), ctypes.sizeof(x))
+
+if __name__ == '__main__':
+    PressKey(0x11)
+    time.sleep(1)
+    ReleaseKey(0x11)
+    time.sleep(1)
--- webCam.py
+++ webCam.py
@@ -1,30 +1,39 @@
+# Step 1. Capturing the image.
+# Step 2. Convert rgb to hsv.
+# Step 3. Gaussian blur to the hsv converted image.
+# Step 4. Morphological transformations to the Gaussian blurred image.
+# Step 5. Searching contours
+# Step 6. Navigate the car.
+
 import cv2
 import imutils
 from imutils.video import VideoStream
 import numpy as np
 from directkeys import A, D, Space, ReleaseKey, PressKey
 
-cam = VideoStream(src = 0).start()
-currenctKey = list()
+cam = VideoStream(src=0).start()
+currentKey=list()
 
 while True:
     key = False
 
     img = cam.read()
-    img = np.flip(img, axis = 1)
+    img = np.flip(img, axis=1)
     img = np.array(img)
 
-    img = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
-    blureed = cv2.GaussianBlur(img,(11,11),0)
-    colourLower = np.array([53,55,209])
-    colourUpper = np.array([180,255,255])
-    mask = cv2.inRange(blureed,colourLower,colourUpper)
-    mask = cv2.morphologyEx(mask,cv2.MORPH_OPEN,np.ones((5,5),np.uint8))
-    mask = cv2.morphologyEx(mask,cv2.MORPH_CLOSE,np.ones((5,5),np.uint8))
+    hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+    blurred = cv2.GaussianBlur(hsv,(11,11),0)
 
-    width = img.shape[0]
-    height = img.shape[1]
-    
+    colourLower = np.array([20, 100, 100])
+    colourUpper = np.array([30, 255, 255])
+
+    mask = cv2.inRange(blurred,colourLower,colourUpper)
+    mask = cv2.morphologyEx(mask, cv2.MORPH_OPEN,np.ones((5,5),np.uint8))
+    mask = cv2.morphologyEx(mask, cv2.MORPH_CLOSE,np.ones((5,5),np.uint8))
+
+    width = img.shape[1]
+    height = img.shape[0]
+
     upContour = mask[0:height//2,0:width]
     downContour = mask[3*height//4:height,2*width//5:3*width//5]
 
@@ -35,37 +44,38 @@ while True:
     cnts_down = imutils.grab_contours(cnts_down)
 
     if len(cnts_up) > 0:
-        c = max(cnts_up,cv2.contourArea)
+        c = max(cnts_up,key = cv2.contourArea)
         M = cv2.moments(c)
-        cx = int(M["m10"]/M["m00"])
+        cX = int(M["m10"]/M["m00"])
 
-        if cx < width//2 - 35:
+        if cX < (width//2 -35):
             PressKey(A)
-            key = True
-            currenctKey.append(A)
+            key=True
+            currentKey.append(A)
         
-        if cx < width//2 + 35:
+        elif cX > (width//2 +35):
             PressKey(D)
-            key = True
-            currenctKey.append(D)
+            key=True
+            currentKey.append(D)
+    
     if len(cnts_down) > 0:
         PressKey(Space)
         key = True
-        currenctKey.append(Space)
-    
-    img = cv2.rectangle(img,(0,0),(width//2 - 35,height//2), (0,255,0),1)
-    cv2.putText(img,'LEFT',(110,30),cv2.FONT_HERSHEY_DUPLEX,1, (139,0,0))
+        currentKey.append(Space)
 
-    img = cv2.rectangle(img,(width//2 + 35, 0),(width,height//2), (0,255,0),1)
-    cv2.putText(img,'RIGHT',(440,30),cv2.FONT_HERSHEY_DUPLEX,1, (139,0,0))
+    img = cv2.rectangle(img, (0,0),(width//2-35,height//2),(0,255,0),1)
+    cv2.putText(img,'LEFT',(110,30),cv2.FONT_HERSHEY_DUPLEX, 1, (139,0,0))
 
-    img = cv2.rectangle(img,(2*(width//5),3*width//4),(3*width//5, height), (0,255,0),1)
-    cv2.putText(img,'NITRO',(110,30),cv2.FONT_HERSHEY_DUPLEX,1,(139,0,0))
+    img = cv2.rectangle(img, (width//2+35,0),(width,height//2),(0,255,0),1)
+    cv2.putText(img,'RIGHT',(440,30),cv2.FONT_HERSHEY_DUPLEX,  1, (139,0,0))
 
-    cv2.imshow("Steering",img)
+    img = cv2.rectangle(img, (2*(width//5),3*height//4),(3*width//5,height),(0,255,0),1)
+    cv2.putText(img,'NITRO',(2*(width//5) + 20,height-10),cv2.FONT_HERSHEY_DUPLEX,  1, (139,0,0))
 
-    if not key and len(currenctKey)!=0:
-        for current in currenctKey:
+    cv2.imshow("Steering", img)
+
+    if not key and len(currentKey)!=0:
+        for current in currentKey:
             ReleaseKey(current)
 
     key = cv2.waitKey(1) & 0xFF
